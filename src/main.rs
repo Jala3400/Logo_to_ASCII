b94baa89030e5820ef84e39049fcf8a0a5b5ecbd
Jala3400 <jala.3400@gmail.com>
2024-10-13 14:46:07 +0200

    Modularization

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
 use clap::Parser;
-use image::ImageBuffer;
-use imageproc::{
-    contours::find_contours, contrast::threshold, drawing::draw_line_segment_mut, point::Point,
+use logo_to_ascii::{
+    abc,
+    args::Args,
+    proc_image::{convert_image, preprocess_image},
 };
-use logo_to_ascii::{abc, args::Args, proc_image::convert_image};
 
 fn main() -> io::Result<()> {
     let mut args: Args = Args::parse();
@@ -13,55 +13,8 @@ fn main() -> io::Result<()> {
     // Load the image
     let mut img = image::open(&args.path).unwrap();
 
-    if args.preprocess || args.border != 0 {
-        let gray_img = img.to_luma8();
-        let bw_image = threshold(&gray_img, args.threshold);
-        if args.border != 0 {
-            let contours = find_contours(&bw_image);
-
-            // Visualize the contours
-            let mut output = ImageBuffer::new(img.width(), img.height());
-
-            // Draw ticker borders
-            let border_thickness = args.border;
-            for contour in contours {
-                let points = &contour.points;
-                for i in 0..points.len() {
-                    let p1: Point<i32> = points[i];
-                    let p2 = points[(i + 1) % points.len()];
-                    for t in 0..border_thickness {
-                        draw_line_segment_mut(
-                            &mut output,
-                            (
-                                (p1.x + t as i32 - border_thickness as i32 / 2) as f32,
-                                p1.y as f32,
-                            ),
-                            (
-                                (p2.x + t as i32 - border_thickness as i32 / 2) as f32,
-                                p2.y as f32,
-                            ),
-                            image::Luma([255u8]),
-                        );
-                        draw_line_segment_mut(
-                            &mut output,
-                            (
-                                p1.x as f32,
-                                (p1.y + t as i32 - border_thickness as i32 / 2) as f32,
-                            ),
-                            (
-                                p2.x as f32,
-                                (p2.y + t as i32 - border_thickness as i32 / 2) as f32,
-                            ),
-                            image::Luma([255u8]),
-                        );
-                    }
-                }
-            }
-
-            img = image::DynamicImage::ImageLuma8(output);
-        } else {
-            img = image::DynamicImage::ImageLuma8(bw_image);
-        }
+    if args.preprocess {
+        img = preprocess_image(img, &args);
     }
 
     if args.all {
--- src/proc_image.rs
+++ src/proc_image.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
 
-use image::GenericImageView;
+use image::{DynamicImage, GenericImageView, ImageBuffer};
+
+use imageproc::{
+    contours::find_contours, contrast::threshold, drawing::draw_line_segment_mut, point::Point,
+};
 
 use crate::{
     args::Args,
     proc_block::{match_group_with_letter, process_block_brightness, process_block_color},
 };
 
-pub fn convert_image(img: image::DynamicImage, args: Args, font: HashMap<char, Vec<Vec<f32>>>) {
+pub fn convert_image(img: DynamicImage, args: Args, font: HashMap<char, Vec<Vec<f32>>>) {
     // Get the dimensions of the image
     let (width, height) = img.dimensions();
 
@@ -42,4 +46,55 @@ fn process_block(
 
     let letter = match_group_with_letter(group, &font);
     print!("{}", letter);
+}
+
+pub fn preprocess_image(img: DynamicImage, args: &Args) -> DynamicImage {
+    let gray_img = img.to_luma8();
+    let bw_image = threshold(&gray_img, args.threshold);
+    if args.border != 0 {
+        let contours = find_contours(&bw_image);
+
+        // Visualize the contours
+        let mut output = ImageBuffer::new(img.width(), img.height());
+
+        // Draw ticker borders
+        let border_thickness = args.border;
+        for contour in contours {
+            let points = &contour.points;
+            for i in 0..points.len() {
+                let p1: Point<i32> = points[i];
+                let p2 = points[(i + 1) % points.len()];
+                for t in 0..border_thickness {
+                    draw_line_segment_mut(
+                        &mut output,
+                        (
+                            (p1.x + t as i32 - border_thickness as i32 / 2) as f32,
+                            p1.y as f32,
+                        ),
+                        (
+                            (p2.x + t as i32 - border_thickness as i32 / 2) as f32,
+                            p2.y as f32,
+                        ),
+                        image::Luma([255u8]),
+                    );
+                    draw_line_segment_mut(
+                        &mut output,
+                        (
+                            p1.x as f32,
+                            (p1.y + t as i32 - border_thickness as i32 / 2) as f32,
+                        ),
+                        (
+                            p2.x as f32,
+                            (p2.y + t as i32 - border_thickness as i32 / 2) as f32,
+                        ),
+                        image::Luma([255u8]),
+                    );
+                }
+            }
+        }
+
+        DynamicImage::ImageLuma8(output)
+    } else {
+        DynamicImage::ImageLuma8(bw_image)
+    }
 }
