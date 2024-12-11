f596a1cbf613db6dedf6fe76335ba4b148d07c85
Jala3400 <jala.3400@gmail.com>
2024-12-11 17:45:17 +0100

    Skip creating a bitmap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use clap::Parser;
+use image::GenericImageView;
 use logo_to_ascii::{
     abc,
     args::Args,
     image_ops::{borders_image, resize},
-    proc_image::{black_and_white, convert_bitmap, get_bitmap},
+    proc_image::convert_image,
 };
 use std::io;
 
 fn main() -> io::Result<()> {
     let mut args: Args = Args::parse();
     args.difference = args.difference % 360;
+
+    // Load the image
+    let mut img = image::open(&args.path).unwrap_or_else(|e| panic!("Failed to open image: {}", e));
 
     if args.all {
         args.chars = (32..=126).map(|c| c as u8 as char).collect::<String>();
@@ -23,9 +27,6 @@ fn main() -> io::Result<()> {
         .collect();
 
     let font = abc::get_dict(&args);
-
-    // Load the image
-    let mut img = image::open(&args.path).unwrap_or_else(|e| panic!("Failed to open image: {}", e));
 
     if args.color || args.border != 0 {
         borders_image(&mut img, &args);
@@ -41,13 +42,44 @@ fn main() -> io::Result<()> {
         resize(&mut img, &mut args);
     }
 
-    let bitmap = if args.preprocess {
-        black_and_white(&img, &args)
-    } else {
-        get_bitmap(&img, &args)
-    };
+    if args.offsetx != 0 || args.offsety != 0 {
+        let (img_width, img_height) = img.dimensions();
+        let new_width = img_width + args.offsetx as u32;
+        let new_height = img_height + args.offsety as u32;
+        let pixel_bytes = img.color().bytes_per_pixel() as usize;
 
-    convert_bitmap(&bitmap, &font, &args);
+        let mut new_bytes = vec![0; (new_width * new_height) as usize * pixel_bytes];
+        let original_bytes = img.into_bytes();
 
+        // Copy original image data with offset
+        for y in 0..img_height {
+            let src_start = (y * img_width) as usize * pixel_bytes;
+            let src_end = src_start + (img_width as usize * pixel_bytes);
+            let dst_start = ((y + args.offsety as u32) * new_width + args.offsetx as u32) as usize
+                * pixel_bytes;
+
+            new_bytes[dst_start..dst_start + (img_width as usize * pixel_bytes)]
+                .copy_from_slice(&original_bytes[src_start..src_end]);
+        }
+
+        img = image::DynamicImage::ImageRgba8(
+            image::RgbaImage::from_raw(new_width, new_height, new_bytes)
+                .expect("Failed to create offset image"),
+        );
+    }
+
+    if args.preprocess {
+        let img_gray = img.to_luma_alpha8();
+        let mut bytes = img_gray.into_raw();
+        for chunk in bytes.chunks_mut(2) {
+            chunk[0] = if chunk[0] > args.threshold { 255 } else { 0 };
+        }
+        img = image::DynamicImage::ImageLumaA8(
+            image::GrayAlphaImage::from_raw(img.width(), img.height(), bytes)
+                .expect("Failed to create black and white image"),
+        );
+    }
+
+    convert_image(&img, &font, &args);
     Ok(())
 }
--- src/proc_image.rs
+++ src/proc_image.rs
@@ -6,6 +6,55 @@ use crate::{
 };
 use image::{DynamicImage, GenericImageView};
 
+pub fn convert_image(img: &DynamicImage, font: &FontBitmap, args: &Args) {
+    let height = img.height() as usize;
+    let width = img.width() as usize;
+
+    let num_groups_x = (width + 7) / 8;
+    let num_groups_y = (height + 15) / 16;
+
+    println!("Image dimensions: {}x{}", width, height);
+    println!("Number of characters: {}x{}", num_groups_x, num_groups_y);
+
+    let mut group = [0f32; 8 * 16];
+    let mut bright_pixels;
+    let mut full_pixels;
+
+    for y in 0..num_groups_y {
+        for x in 0..num_groups_x {
+            bright_pixels = 0;
+            full_pixels = 0;
+            for by in 0..16 {
+                let iy = y * 16 + by;
+                for bx in 0..8 {
+                    let ix = x * 8 + bx;
+                    let cords_block = by * 8 + bx;
+                    if iy < height && ix < width {
+                        let pixel = img.get_pixel(ix as u32, iy as u32);
+                        group[cords_block] = calc_custom_brightness(&pixel, args);
+                        if group[cords_block] > -args.midpoint_brightness {
+                            bright_pixels += 1;
+                            full_pixels +=
+                                (group[cords_block] >= 1.0 - args.midpoint_brightness) as usize;
+                        }
+                    } else {
+                        group[cords_block] = -args.midpoint_brightness;
+                    }
+                }
+            }
+            print!(
+                "{}",
+                if full_pixels == 16 * 8 {
+                    font.data.last().unwrap().char
+                } else {
+                    match_group_with_letter(&group, font, bright_pixels)
+                }
+            );
+        }
+        println!();
+    }
+}
+
 pub fn convert_bitmap(bitmap: &Bitmap, font: &FontBitmap, args: &Args) {
     let height = bitmap.height;
     let width = bitmap.width;
@@ -14,7 +63,7 @@ pub fn convert_bitmap(bitmap: &Bitmap, font: &FontBitmap, args: &Args) {
     let num_groups_y = (height + 15) / 16;
 
     println!("Image dimensions: {}x{}", width, height);
-    println!("Number of 8x16 groups: {}x{}", num_groups_x, num_groups_y);
+    println!("Number of characters: {}x{}", num_groups_x, num_groups_y);
 
     let mut group = [0f32; 8 * 16];
     let mut bright_pixels;
