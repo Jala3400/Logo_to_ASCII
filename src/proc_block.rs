f19af6ec2988d7e4e778be0b337fb5dc2be275ea
Jala3400 <jala.3400@gmail.com>
2025-09-20 20:14:45 +0200

    Renamed group to block

--- src/proc_block.rs
+++ src/proc_block.rs
@@ -1,20 +1,20 @@
 use crate::types::{Algorithm, FontBitmap};
 
 // Matches a block of pixels with a character
-pub fn match_group_with_letter(
-    group: &[f32; 8 * 16],
+pub fn match_block_with_letter(
+    block: &[f32; 8 * 16],
     font: &FontBitmap,
     bright_blocks: usize,
     algorithm: &Algorithm,
 ) -> char {
     match algorithm {
-        Algorithm::MaxMult => max_mult(group, font, bright_blocks),
-        Algorithm::MinDiff => min_diff(group, font),
-        Algorithm::MinDiffSq => min_diff_sq(group, font),
+        Algorithm::MaxMult => max_mult(block, font, bright_blocks),
+        Algorithm::MinDiff => min_diff(block, font),
+        Algorithm::MinDiffSq => min_diff_sq(block, font),
     }
 }
 
-fn max_mult(group: &[f32; 8 * 16], font: &FontBitmap, bright_blocks: usize) -> char {
+fn max_mult(block: &[f32; 128], font: &FontBitmap, bright_blocks: usize) -> char {
     let mut best_match = font.data[0].char;
     let mut best_match_value = f32::MIN;
 
@@ -23,7 +23,7 @@ fn max_mult(group: &[f32; 8 * 16], font: &FontBitmap, bright_blocks: usize) -> c
         let mut match_value = 0.0;
         for i in 0..128 {
             // Add the value of the pixel to the match value (the block brightness multiplied by the character's brightness in a given position)
-            match_value += group[i] * letter.data[i];
+            match_value += block[i] * letter.data[i];
         }
 
         // If the match value is greater than the best match value, update the best match
@@ -36,14 +36,14 @@ fn max_mult(group: &[f32; 8 * 16], font: &FontBitmap, bright_blocks: usize) -> c
     best_match
 }
 
-fn min_diff(group: &[f32; 8 * 16], font: &FontBitmap) -> char {
+fn min_diff(block: &[f32; 128], font: &FontBitmap) -> char {
     let mut best_match = font.data[0].char;
     let mut less_diffrence = f32::MAX;
 
     for letter in &font.data {
         let mut match_value = 0.0;
         for i in 0..128 {
-            match_value += (group[i] - letter.data[i]).abs();
+            match_value += (block[i] - letter.data[i]).abs();
         }
 
         if match_value < less_diffrence {
@@ -55,14 +55,14 @@ fn min_diff(group: &[f32; 8 * 16], font: &FontBitmap) -> char {
     best_match
 }
 
-fn min_diff_sq(group: &[f32; 8 * 16], font: &FontBitmap) -> char {
+fn min_diff_sq(block: &[f32; 128], font: &FontBitmap) -> char {
     let mut best_match = font.data[0].char;
     let mut less_diffrence = f32::MAX;
 
     for letter in &font.data {
         let mut match_value = 0.0;
         for i in 0..128 {
-            let diff = group[i] - letter.data[i];
+            let diff = block[i] - letter.data[i];
             match_value += diff * diff;
         }
 
