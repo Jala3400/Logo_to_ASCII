4e726a6e3e5674af2363ef1925c5388cadbbcbf5
Jala3400 <jala.3400@gmail.com>
2025-12-14 18:31:57 +0100

    Better help message, now with sections

--- src/args.rs
+++ src/args.rs
@@ -6,117 +6,178 @@ use clap::Parser;
 pub struct Args {
     // Help
     /// Print the help message
-    #[arg(long, action = clap::ArgAction::HelpLong)]
+    #[arg(long, action = clap::ArgAction::HelpLong, help_heading = "Help")]
     help: Option<bool>,
 
     // Input/Output
     /// Path of the image to process
-    #[arg()]
+    #[arg(help_heading = "Input/Output")]
     pub path: String,
 
     /// Output the image to a file
-    #[arg(short, long)]
+    #[arg(short, long, help_heading = "Input/Output")]
     pub output: Option<String>,
 
     /// Path of the font to use (optional)
-    #[arg(short, long)]
+    #[arg(short, long, help_heading = "Input/Output")]
     pub font: Option<String>,
 
     // Character Set
     /// Characters used to convert the image
-    #[arg(long, default_value = "8dbqp'·. ")]
+    #[arg(long, default_value = "8dbqp'·. ", help_heading = "Character Set")]
     pub chars: String,
 
     /// Add characters to the default ones
-    #[arg(short, long, default_value = "")]
+    #[arg(short, long, default_value = "", help_heading = "Character Set")]
     pub add_chars: String,
 
     /// Use characters except:
-    #[arg(short = 'x', long, default_value = "")]
+    #[arg(short = 'x', long, default_value = "", help_heading = "Character Set")]
     pub except: String,
 
     /// Use all ASCII printable characters to convert the image
-    #[arg(long, default_value_t = false)]
+    #[arg(long, default_value_t = false, help_heading = "Character Set")]
     pub all: bool,
 
     // Image Processing
     /// Inverse the brightness of the image (transparent is never printed)
-    #[arg(short, long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help_heading = "Image Processing"
+    )]
     pub negative: bool,
 
     /// Preprocess the image to black and white.
     /// Makes the transparent pixels black by default.
-    #[arg(short = 'r', long = "bw", default_value_t = false)]
+    #[arg(
+        short = 'r',
+        long = "bw",
+        default_value_t = false,
+        help_heading = "Image Processing"
+    )]
     pub black_and_white: bool,
 
     /// Threshold value for the black and white conversion (from 0 to 255)
-    #[arg(short, long, default_value_t = 0.5)]
+    #[arg(short, long, default_value_t = 0.5, help_heading = "Image Processing")]
     pub threshold: f32,
 
     /// Makes transparent pixels visible
-    #[arg(short, long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help_heading = "Image Processing"
+    )]
     pub visible: bool,
 
     /// Saturate the image
-    #[arg(short = 's', long, default_value_t = false)]
+    #[arg(
+        short = 's',
+        long,
+        default_value_t = false,
+        help_heading = "Image Processing"
+    )]
     pub saturate: bool,
 
     /// Grayscale and brighten the image
-    #[arg(short = 'g', long, default_value_t = false)]
+    #[arg(
+        short = 'g',
+        long,
+        default_value_t = false,
+        help_heading = "Image Processing"
+    )]
     pub grayscale: bool,
 
     /// Midpoint of the brightness spectrum
-    #[arg(short, long, default_value_t = 0.5)]
+    #[arg(short, long, default_value_t = 0.5, help_heading = "Image Processing")]
     pub midpoint_brightness: f32,
 
     // Dimensions and Offsets
     /// Number of characters in the width of the end image (0 to default)
-    #[arg(short = 'w', long = "cw", default_value_t = 0)]
+    #[arg(
+        short = 'w',
+        long = "cw",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub char_width: u32,
 
     /// Number of characters in the height of the end image (0 to default)
-    #[arg(short = 'h', long = "ch", default_value_t = 0)]
+    #[arg(
+        short = 'h',
+        long = "ch",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub char_height: u32,
 
     /// Actual width of the image (0 to default)
-    #[arg(long = "pw", default_value_t = 0)]
+    #[arg(
+        long = "pw",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub pixel_width: u32,
 
     /// Actual height of the image (0 to default)
-    #[arg(long = "ph", default_value_t = 0)]
+    #[arg(
+        long = "ph",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub pixel_height: u32,
 
     /// Offsetx of the width of the image
-    #[arg(long = "ofx", default_value_t = 0)]
+    #[arg(
+        long = "ofx",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub offsetx: usize,
 
     /// Offsety of the height of the image
-    #[arg(long = "ofy", default_value_t = 0)]
+    #[arg(
+        long = "ofy",
+        default_value_t = 0,
+        help_heading = "Dimensions and Offsets"
+    )]
     pub offsety: usize,
 
     // Borders and Colors
     /// Separates colors (change thickness with `-b`)
-    #[arg(short, long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help_heading = "Borders and Colors"
+    )]
     pub color_borders: bool,
 
     /// Detect borders measuring brightness (when not used with color) (0 to disable)
-    #[arg(short, long, default_value_t = 0)]
+    #[arg(short, long, default_value_t = 0, help_heading = "Borders and Colors")]
     pub border: u32,
 
     /// Threshold for the color difference (from 0 to 360) (if used for brightness, it will be divided by 360 automatically)
-    #[arg(short, long, default_value_t = 30)]
+    #[arg(short, long, default_value_t = 30, help_heading = "Borders and Colors")]
     pub difference: u16,
 
     /// Print the image with colors
-    #[arg(short = 'C', long, default_value_t = false)]
+    #[arg(
+        short = 'C',
+        long,
+        default_value_t = false,
+        help_heading = "Borders and Colors"
+    )]
     pub print_color: bool,
 
     // Algorithm and Misc
     /// Algorithm used to match blocks to characters
-    #[arg(long = "algo", value_enum, default_value_t = Algorithm::MaxMult)]
+    #[arg(long = "algo", value_enum, default_value_t = Algorithm::MaxMult, help_heading = "Algorithm and Misc")]
     pub algorithm: Algorithm,
 
     /// Print information about the image
-    #[arg(long, default_value_t = false)]
+    #[arg(long, default_value_t = false, help_heading = "Algorithm and Misc")]
     pub verbose: bool,
 }
